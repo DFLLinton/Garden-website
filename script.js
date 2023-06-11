12d841fb1ed40fcc097039551dcf298bb520cd9b
DFLLinton <dominiclinton@gmail.com>
2023-06-11 10:19:22 +0100

    Working carousel

--- script.js
+++ script.js
@@ -13,25 +13,37 @@ const contact = document.getElementById('contactpara');
         contact.style.display = "none";
     }
 }
-
+const images =[
+    'garden1.jpeg',
+    'garden2.jpeg',
+    'garden3.jpeg',
+    'garden4.jpeg'
+]
 function changeImage(x){
-    const images ={
-        1: 'garden1.jpeg',
-        2: 'garden2.jpeg',
-        3: 'garden3.jpeg',
-        4: 'garden4.jpeg',
-    }
-    let count = 1;
+
     if (x == "about"){
-        document.body.style.backgroundImage = `url(${images['2']})`;
+        document.body.style.backgroundImage = `url(${images['1']})`;
         }else if (x == "contact"){
-            document.body.style.backgroundImage = `url(${images['3']})`;
-        }else if (x == "left"){
-            document.body.style.backgroundImage = `url(${images[`${count - 1}`]})`;
-        }else if (x == "right"){
-            document.body.style.backgroundImage = `url(${images[`${count + 1}`]})`;
+            document.body.style.backgroundImage = `url(${images['2']})`;
         }else{
-            document.body.style.backgroundImage = `url(${images['1']})`;
+            document.body.style.backgroundImage = `url(${images['0']})`;
         }
 }
 
+let currentIndex = 1
+
+function nextImageSlide(){
+currentIndex++;
+if (currentIndex >= images.length) {
+    currentIndex = 0;
+  }
+  document.body.style.backgroundImage = `url(${images[currentIndex]})`
+} 
+
+function prevImageSlide(){
+    currentIndex--;
+    if (currentIndex < 1) {
+        currentIndex = images.length -1;
+      }
+      document.body.style.backgroundImage = `url(${images[currentIndex]})`
+    } 
